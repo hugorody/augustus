216400c0df79ff1332ff53f22efeeac3c3e51a57
Hugo Rody <hugo.rody@unifesp.br>
2017-03-15 17:34:13 -0300

    Update pipeline_augustus.sh

--- pipeline_augustus.sh
+++ pipeline_augustus.sh
@@ -85,6 +85,11 @@ rm *.aa
 
 python myoverlap.py augustus.overlap2 > augustus.overlap3
 
+#prepara o arquivo
+sed -i "s/\[//g" augustus.overlap3
+sed -i "s/\]//g" augustus.overlap3
+sed -i "s/'//g" augustus.overlap3
+sed -i "s/,//g" augustus.overlap3
 
 #Convert GFF master and wholeoverlap5 to final file overlap6
 #input: GFF master (join all the 25 augustus gff files) and wholeoverlap5.txt
